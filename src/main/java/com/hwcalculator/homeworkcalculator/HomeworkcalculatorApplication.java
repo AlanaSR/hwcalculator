ec9a373cbea18272597991ed66400929db9039b0
AlanaSR <svetlanka.shkola@gmail.com>
2024-11-04 13:29:00 +0500

    initialization

---
+++ src/main/java/com/hwcalculator/homeworkcalculator/HomeworkcalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.hwcalculator.homeworkcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkcalculatorApplication.class, args);
+	}
+
+}
